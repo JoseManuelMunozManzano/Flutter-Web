475da0e4397482b0289ba9c43c92576fbc7ac45e
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2025-02-23 05:38:17 +0100

    Primeros pasos - Creando un menú de navegación

--- bases_web/lib/ui/pages/counter_page.dart
+++ bases_web/lib/ui/pages/counter_page.dart
@@ -1,3 +1,4 @@
+import 'package:bases_web/ui/shared/custom_app_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:bases_web/ui/shared/custom_flat_button.dart';
 
@@ -18,9 +19,30 @@ class _CounterPageState extends State<CounterPage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'Contador: $counter',
-            style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+
+          // El Spacer() nos ayuda a expandir el contenido.
+          // Esto hace que CustomAppMenu() se sitúe en la parte
+          // superior del navegador.
+          // Sin el se coloca en el centro.
+          // To-do el contador se posiciona al final del navegador.
+          // Pero para evitar esto último, hemos colocado otro Spacer()
+          // después del Widget Row.
+          CustomAppMenu(),
+          Spacer(),
+
+          // Con este Widget, el texto se empieza a adaptar basado en el
+          // espacio disponible (Responsive)
+          // Para que el texto no quede muy pegado a los bordes usamos
+          // un padding.
+          FittedBox(
+            fit: BoxFit.contain,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'Contador: $counter',
+                style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+              ),
+            ),
           ),
 
           Row(
@@ -37,6 +59,8 @@ class _CounterPageState extends State<CounterPage> {
               ),
             ],
           ),
+
+          Spacer(),
         ],
       ),
     );
