89c78ea427794e6157e08f524a9f774e735c8a7d
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2025-02-23 10:37:11 +0100

    Primeros pasos - Código específico para la web

--- bases_web/lib/ui/router/route_generator.dart
+++ bases_web/lib/ui/router/route_generator.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'package:bases_web/ui/pages/counter_page.dart';
 import 'package:bases_web/ui/pages/counter_provider_page.dart';
@@ -6,10 +9,8 @@ import 'package:bases_web/ui/pages/page_404.dart';
 
 // Clase que va a trabajar en la creación de todas las rutas de manera automática.
 class RouteGenerator {
-
   static Route<dynamic> generateRoute(RouteSettings settings) {
-
-    switch(settings.name) {
+    switch (settings.name) {
       case '/stateful':
         return _fadeRoute(CounterPage(), '/provider');
       case '/provider':
@@ -21,7 +22,6 @@ class RouteGenerator {
 
   // Cambiamos la forma de transición entre rutas.
   static PageRoute _fadeRoute(Widget child, String routeName) {
-
     return PageRouteBuilder(
       // Para recuperar todo nuestro URL tenemos que trabajar con nuestros settings.
       // Los arguments son los de la ruta. Podemos mandar un objeto...
@@ -29,17 +29,20 @@ class RouteGenerator {
       settings: RouteSettings(name: routeName),
       pageBuilder: (_, __, ___) => child,
       transitionDuration: Duration(milliseconds: 200),
-      transitionsBuilder: (_, animation, __, ___) =>
-        // Si no queremos transición, en vez de FadeTransition, mandamos el child y ya está.
-        // child,
-        //
-        // O, con la transición.
-        // Vamos a dejarlo con la transición por el efecto que haremos luego, llamado Giro Animation,
-        // que trabaja en base a la duración de la transición (arriba)
-        FadeTransition(
-          opacity: animation,
-          child: child,
-        )
+      transitionsBuilder:
+          (_, animation, __, ___) =>
+          // Si estamos en web, devolvemos esto:
+          (kIsWeb) ?
+          //
+          // Si no queremos transición, en vez de FadeTransition, mandamos el child y ya está.
+          // child,
+          //
+          // O, con la transición.
+          // Vamos a dejarlo con la transición por el efecto que haremos luego, llamado Giro Animation,
+          // que trabaja en base a la duración de la transición (arriba)
+          FadeTransition(opacity: animation, child: child)
+          // Sino estamos en web, devolvemos esto:
+          : CupertinoPageTransition(primaryRouteAnimation: animation, secondaryRouteAnimation: __, linearTransition: true, child: child)
     );
   }
 }
