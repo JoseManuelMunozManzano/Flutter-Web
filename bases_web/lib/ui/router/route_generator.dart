752a611057583f4a1051ecc62cadd92d4448ec22
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2025-02-23 10:25:37 +0100

    Primeros pasos - Transición entre páginas y URL

--- bases_web/lib/ui/router/route_generator.dart
+++ bases_web/lib/ui/router/route_generator.dart
@@ -11,11 +11,35 @@ class RouteGenerator {
 
     switch(settings.name) {
       case '/stateful':
-        return MaterialPageRoute(builder: (_) => CounterPage());
+        return _fadeRoute(CounterPage(), '/provider');
       case '/provider':
-        return MaterialPageRoute(builder: (_) => CounterProviderPage());
+        return _fadeRoute(CounterProviderPage(), '/provider');
       default:
-        return MaterialPageRoute(builder: (_) => Page404());
+        return _fadeRoute(Page404(), '/404');
     }
-  } 
+  }
+
+  // Cambiamos la forma de transición entre rutas.
+  static PageRoute _fadeRoute(Widget child, String routeName) {
+
+    return PageRouteBuilder(
+      // Para recuperar todo nuestro URL tenemos que trabajar con nuestros settings.
+      // Los arguments son los de la ruta. Podemos mandar un objeto...
+      // Por ahora solo nos hace falta el nombre de la ruta, para que aparezca en la URL.
+      settings: RouteSettings(name: routeName),
+      pageBuilder: (_, __, ___) => child,
+      transitionDuration: Duration(milliseconds: 200),
+      transitionsBuilder: (_, animation, __, ___) =>
+        // Si no queremos transición, en vez de FadeTransition, mandamos el child y ya está.
+        // child,
+        //
+        // O, con la transición.
+        // Vamos a dejarlo con la transición por el efecto que haremos luego, llamado Giro Animation,
+        // que trabaja en base a la duración de la transición (arriba)
+        FadeTransition(
+          opacity: animation,
+          child: child,
+        )
+    );
+  }
 }
